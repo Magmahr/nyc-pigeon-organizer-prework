738299d31797152462764f59b192742876da4c4a
Matt Maher <mattmaher0@gmail.com>
2018-05-22 18:57:42 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized = {}
+  data.each do |description, sec_description| 
+    sec_description.each do |key, name|
+      name.each do |el| 
+        organized[el] = {} 
+      end  
+    end
+  end
+organized.each do |bird, stats|
+  data.each do |key, value|
+    value.each do |attribute, array|
+        names_array = []
+        names_array << attribute.to_s if array.include?(bird)
+        stats[key] = names_array 
+    end
+  end
+end  
+  organized
 end
