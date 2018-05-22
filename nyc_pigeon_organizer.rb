ab6c73d76854d0a60089b0ae6ef0f89872302b90
Matt Maher <mattmaher0@gmail.com>
2018-05-22 19:00:38 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,13 @@
 
 
 def nyc_pigeon_organizer(data)
-  organized = {}
-  data.each do |description, sec_description| 
-    sec_description.each do |key, name|
-      name.each do |el| 
-        organized[el] = {} 
+  data.each_with_object({}) do |(key, v), result|
+    v.each do |value, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << value.to_s
       end  
-    end
-  end
-organized.each do |bird, stats|
-  data.each do |key, value|
-    value.each do |attribute, array|
-        names_array = []
-        names_array << attribute.to_s if array.include?(bird)
-        stats[key] = names_array 
-    end
-  end
-end  
-  organized
+    end 
+  end  
 end
